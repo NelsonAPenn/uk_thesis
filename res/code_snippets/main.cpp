5ee9652cef2e065c37c6d0990e2caa8c928a9b90
Nelson Penn <nelsonapenn@protonmail.com>
2021-07-08 21:09:54 -0400

    Added code example

---
+++ res/code_snippets/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // Man I love super long lines. Line breaks almost physically hurt me. I like having to side scroll way way way way way off the side of the page to make code edits. 80 character line limits are intended for children who are learning to code.
+
+    std::cout << "Enter name:\n";
+    std::string name; std::cin >> name;
+
+    std::cout << "~~~ " << name << ": Professional Cat Petter ~~~\n";
+
+}
